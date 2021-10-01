70656e99e32d5e21f0f0fe64957e62d391980b1c
Charles King <kingcsb2000@gmail.com>
2021-10-01 10:10:14 +0800

    Finished raw mode configs

--- dterm.c
+++ dterm.c
@@ -1,15 +1,44 @@
 // Project Dods Term - A terminal-based text editor written in C
 
+/* ==================== INCLUDES ============================================*/
+
 #include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
 #include <termios.h>
 #include <unistd.h>
 #include <stdlib.h>
 
+/*==============================END SECTION===================================*/
+
+
+
+
+
+/*==================================DATA======================================*/
+
 struct termios orig_termios; /* Declare a copy of termios struct to preserve it for future use */
 
+/*==============================END SECTION===================================*/
+
+
+
+
+/*============================TERMINAL========================================*/
+
+void die(const char* s){
+	/*======================================
+	Function to print error messages when
+	program encounters errors
+	======================================*/
+	perror(s); /* stdio.h */
+	exit(1); /* stdlib.h */
+}
+
 void disableRawMode(){
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+	if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1){
+		die("tcsetattr");
+	}
 
 	/*=====================================
 	TCSAFLUSH -> termios.h
@@ -22,13 +51,35 @@ void enableRawMode(){
 	/*=======================================
 	Function to turn off input echoing
 	=======================================*/
-
-	tcgetattr(STDIN_FILENO, &orig_termios);
+	if(tcgetattr(STDIN_FILENO, &orig_termios) == -1){
+		die("tcgetattr");
+	}
+	
 	atexit(disableRawMode); /* stdlib.h -> calls attributes when program ends */
 
 	struct termios raw = orig_termios; /* create termios struct raw and assign orig */
 
-	raw.c_lflag &= ~(ECHO | ICANON | ISIG); 
+	/* Terminal Flags */
+	raw.c_iflag &= ~(IXON | ICRNL | BRKINT | INPCK | ISTRIP);
+	raw.c_oflag &= ~(OPOST);
+	raw.c_cflag |= (CS8);
+	raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN); 
+
+	/* Terminal Control */
+	raw.c_cc[VMIN] = 0;
+	raw.c_cc[VTIME] = 1;
+
+	/*=======================================
+	VMIN, VTIME -> termios.h
+
+	VMIN - sets the min number of bytes needed
+	before read can return 
+
+	VTIME - sets max amt of time to wait
+	before read returns
+	========================================*/
+
+
 
 	/*=======================================
 	Bitwise NOT the ECHO and ICANON value of the terminal's flags and bitwise AND to assign it to raw's local flags
@@ -41,23 +92,58 @@ void enableRawMode(){
 	will be read byte-by-byte instead of
 	line-by-line (e.g. No need to press
 	enter to register input)
+
+	IXON - Input flag + XON (Ctrl+S, Ctrl + Q)
+	which the frmr stops transmission of data
+	and the ltr resumes transmission
+
+	ICRNL - flag to make ctrl+m and enter carriage return
+
+	IEXTEN - flag to disable ctrl+v and ctrl+o
+
+	OPOST - flag to turn off output processing
+	such as adding \n\r to new line
+
+	## Legacy flags 
+
+	BRKINT - turn off break condition much like ctrl+c
+
+	INPCK - enables parity checkng
+
+	ISTRIP - causes 8th bit of each input byte
+	to be stripped (e.g. set to 0). Most probably
+	off by default
+
+	CS8 - not a flag but a bit mask which sets
+	char size to 8 bits per byte
 	=======================================*/
 
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+	if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1){
+		die("tcgetattr");
+	}
 }
 
+/*=====================END SECTION===================================*/
 
+
+
+
+
+/*============================INIT===================================*/
 int main(){
 	enableRawMode(); /* While program is running, no input will be echoed */
-
-
-	char c;
-	while(read(STDIN_FILENO, &c, 1 && c != 'q') == 1){
-		if(iscntrl(c)){
-			printf("%d\n", c);
-		} else {
-			printf("%d ('%c')\n", c, c);
+	
+	while(1){
+		char c = '\0';
+		if(read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN){
+			die("read");
 		}
+		if(iscntrl(c)){
+			printf("%d\r\n", c);
+		} else {
+			printf("%d ('%d')", c, c);
+		}
+		if(c == 'q') break;
 	}
 	
 	// While able to read or input not q
@@ -74,3 +160,5 @@ int main(){
 
 	return 0;
 }
+
+/*=====================END SECTION===================================*/
